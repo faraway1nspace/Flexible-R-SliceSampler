fab82581d74cf093eb2ff9832fce7dfc13691de4
faraway1nspace <robertw.rankin@gmail.com>
2023-04-27 19:28:22 -0400

    added thinning to mcmc

--- src/flexible_slice_sampler.R
+++ src/flexible_slice_sampler.R
@@ -5,6 +5,7 @@ slice.sample <- function(x.init, # initial estimates of variables
                          data_likelihood, # y data and model-matrices
                          prior_parameters_list, # hyperparameters for priors, per variable in x
                          nslice=2000,
+                         thin=1,
                          x.lowb=rep(-10^6, length(x.init)),
                          x.uppb=rep(10^6, length(x.init)),
                          w=rep(3.0,length(x.init)),
@@ -45,134 +46,138 @@ slice.sample <- function(x.init, # initial estimates of variables
     # loop through n-slices/samples
     for(i in 1:nslice){
 
+        for(j in 1:thin){
+
         # loop through each parameter p: we sample univariately, conditional on other parameters
-        for(p in 1:npar){ # 
+            for(p in 1:npar){ # 
 
-            # target density for this parameter p
-            log_density_for_p = list_of_log_posteriors[[p]]
+                # target density for this parameter p
+                log_density_for_p = list_of_log_posteriors[[p]]
             
-            # get (posterio) density at x=x
-            log_f_at_x <- log_density_for_p(
-                x_target=x[[p]],                
-                x_all=x,
-                data_likelihood,
-                prior_parameters_list[[p]]
-            )
-            
-            # sample random slice height < f(x) y.r
-            random_slice_height <- log_f_at_x - rexp(1,1)
-
-            # sample window size (adjustment factor to grow L)
-            uv <- runif(2,0,1)
-
-            # R and L are boundaries within-which we sample a new value for x
-            # ... we initiaize R and L to the most recent values of ALL parameters
-            L <- R <- x # notice we condition on the other parameters in
-
-            # move p's L to the left by w[p]*uv[1] (i.e., move outside density)
-            L[p] <- pmax( x[p]-w[p]*uv[1], x.lowb[p]);
-            # log-density at L[p]
-            f_at_L <- log_density_for_p(
-                x_target=L[[p]],
-                x_all=L,
-                data_likelihood,
-                prior_parameters_list[[p]]
-            )
-
-            # move p's R to the right by the window size w[p]
-            R[p] <- pmin(L[[p]]+w[[p]], x.uppb[[p]])
-            J <- floor(m*uv[2]) # J steps to move L
-            K <- (m-1)-J # K steps to move R
-
-            # GOAL: adjust L[p] and R[p] to be at the boundards of the log-density
-            # step 1: incrementally move L[p] outside of density until f_at_L < slice_height
-            while(
-                (random_slice_height < f_at_L) & (J>0) & (L[p]>x.lowb[p])
-            ){
-                # decrease L[p] by w[p] 
-                L[p] <- pmax(L[p]-w[p], x.lowb[p])
-                # continue for J steps
-                J <- J-1
-                # new log-density at L
-                f_at_L <- log_density_for_p(
-                    x_target=L[p],
-                    x=L,
+                # get (posterio) density at x=x
+                log_f_at_x <- log_density_for_p(
+                    x_target=x[[p]],                
+                    x_all=x,
                     data_likelihood,
                     prior_parameters_list[[p]]
                 )
-            } # end when f_at_L < slice_height or J==0
-
-            # step 2: incrementally move R[p] outside of density until f_at_R < slice_height
-            f_at_R <- log_density_for_p(
-                x_target=R[[p]],
-                x_all=R,
-                data_likelihood,
-                prior_parameters_list[[p]]
-            )
-
-            # while loop to adjust R[p] by W[p]
-            while(
-                (random_slice_height < f_at_R) & (K>0) & (R[p]<x.uppb[p])
-            ){
-                # increment R[p] by w[p] (i.e., push rightward until outside of density
-                R[p] <- pmin(R[p]+w[p], x.uppb[p])
-                # decrement K
-                K <- K-1
-                # new log-density at R
-                f_at_R<- log_density_for_p(
-                    x=R, x_target=R[p], data_likelihood, prior_parameters_list[[p]]
-                )
-            } # end when f_at_R < slice_height or K==0
-
-            # Step 3: sample between L and R
-            xstar <- x 
-            # insert new sample at p: sample xstar[p] from Uniform(L[p], R[p])
-            xstar[p] <- runif(1,L[p],R[p])
-
-            # log-denisty at the new x-star
-            f_at_xstar <- log_density_for_p(
-                x_target=xstar[[p]],
-                x_all=xstar,
-                data_likelihood,
-                prior_parameters_list[[p]]
-            )
-            # we accept x if log_density_at_x is > random_slice_height            
-            accept_f_at_xstar <- (f_at_xstar >= random_slice_height)
             
-            # if f is not > random_slice_height, then we adjust L and R until it is
-            pass.counter.i <- pass.counter
-            pass <-  accept_f_at_xstar & pass.counter.i>0
-            # if not pass, we micro-adjust L and R
-            while(!pass){
+                # sample random slice height < f(x) y.r
+                random_slice_height <- log_f_at_x - rexp(1,1)
 
-                # new L is position of X[p]
-                L[p] <- xstar[p]*(xstar[p]<x[p])+ L[p]*(xstar[p]>=x[p])
-                # new R is position of X[p]
-                R[p] <- xstar[p]*(xstar[p]>x[p])+ R[p]*(xstar[p]<=x[p])
-                
-                # sample new x[p] from between L and R
+                # sample window size (adjustment factor to grow L)
+                uv <- runif(2,0,1)
+
+                # R and L are boundaries within-which we sample a new value for x
+                # ... we initiaize R and L to the most recent values of ALL parameters
+                L <- R <- x # notice we condition on the other parameters in
+
+                # move p's L to the left by w[p]*uv[1] (i.e., move outside density)
+                L[p] <- pmax( x[p]-w[p]*uv[1], x.lowb[p]);
+                # log-density at L[p]
+                f_at_L <- log_density_for_p(
+                    x_target=L[[p]],
+                    x_all=L,
+                    data_likelihood,
+                    prior_parameters_list[[p]]
+                )
+
+                # move p's R to the right by the window size w[p]
+                R[p] <- pmin(L[[p]]+w[[p]], x.uppb[[p]])
+                J <- floor(m*uv[2]) # J steps to move L
+                K <- (m-1)-J # K steps to move R
+
+                # GOAL: adjust L[p] and R[p] to be at the boundards of the log-density
+                # step 1: incrementally move L[p] outside of density until f_at_L < slice_height
+                while(
+                    (random_slice_height < f_at_L) & (J>0) & (L[p]>x.lowb[p])
+                ){
+                    # decrease L[p] by w[p] 
+                    L[p] <- pmax(L[p]-w[p], x.lowb[p])
+                    # continue for J steps
+                    J <- J-1
+                    # new log-density at L
+                    f_at_L <- log_density_for_p(
+                        x_target=L[p],
+                        x=L,
+                        data_likelihood,
+                        prior_parameters_list[[p]]
+                    )
+                } # end when f_at_L < slice_height or J==0
+
+                # step 2: incrementally move R[p] outside of density until f_at_R < slice_height
+                f_at_R <- log_density_for_p(
+                    x_target=R[[p]],
+                    x_all=R,
+                    data_likelihood,
+                    prior_parameters_list[[p]]
+                )
+
+                # while loop to adjust R[p] by W[p]
+                while(
+                    (random_slice_height < f_at_R) & (K>0) & (R[p]<x.uppb[p])
+                ){
+                    # increment R[p] by w[p] (i.e., push rightward until outside of density
+                    R[p] <- pmin(R[p]+w[p], x.uppb[p])
+                    # decrement K
+                    K <- K-1
+                    # new log-density at R
+                    f_at_R <- log_density_for_p(
+                        x=R, x_target=R[p], data_likelihood, prior_parameters_list[[p]]
+                    )
+                } # end when f_at_R < slice_height or K==0
+
+                # Step 3: sample between L and R
+                xstar <- x 
+                # insert new sample at p: sample xstar[p] from Uniform(L[p], R[p])
                 xstar[p] <- runif(1,L[p],R[p])
 
-                # log-density at start
+                # log-denisty at the new x-star
                 f_at_xstar <- log_density_for_p(
-                    x_target=xstar[p],
+                    x_target=xstar[[p]],
                     x_all=xstar,
                     data_likelihood,
                     prior_parameters_list[[p]]
                 )
                 # we accept x if log_density_at_x is > random_slice_height            
                 accept_f_at_xstar <- (f_at_xstar >= random_slice_height)
-                # decrement pass.counter.i
-                pass <- accept_f_at_xstar & pass.counter.i>0
-                pass.counter.i <- pass.counter.i-1
-                if(pass.counter.i<0){stop(sprintf('failed to get xstar; something wrong with %d',p))}
-            } # done
+                
+                # if f is not > random_slice_height, then we adjust L and R until it is
+                pass.counter.i <- pass.counter
+                pass <-  accept_f_at_xstar & pass.counter.i>0
+                # if not pass, we micro-adjust L and R
+                while(!pass){
 
-            # update xstar to x
-            x <- xstar
+                    # new L is position of X[p]
+                    L[p] <- xstar[p]*(xstar[p]<x[p])+ L[p]*(xstar[p]>=x[p])
+                    # new R is position of X[p]
+                    R[p] <- xstar[p]*(xstar[p]>x[p])+ R[p]*(xstar[p]<=x[p])
+                    
+                    # sample new x[p] from between L and R
+                    xstar[p] <- runif(1,L[p],R[p])
 
-            # monitor our slice of R[p]-L[p] (we'll use it to dynamically adjust w)
-            w_monitor_slice_size[p] <- R[p]-L[p]
+                    # log-density at start
+                    f_at_xstar <- log_density_for_p(
+                        x_target=xstar[p],
+                        x_all=xstar,
+                        data_likelihood,
+                        prior_parameters_list[[p]]
+                    )
+                    # we accept x if log_density_at_x is > random_slice_height            
+                    accept_f_at_xstar <- (f_at_xstar >= random_slice_height)
+                                        # decrement pass.counter.i
+                    pass <- accept_f_at_xstar & pass.counter.i>0
+                    pass.counter.i <- pass.counter.i-1
+                    if(pass.counter.i<0){stop(sprintf('failed to get xstar; something wrong with %d',p))}
+                } # done
+
+                # update xstar to x
+                x <- xstar
+
+                # monitor our slice of R[p]-L[p] (we'll use it to dynamically adjust w)
+                w_monitor_slice_size[p] <- R[p]-L[p]
+                
+            } # thin
             
         } # p
         # done loopting through all parameters p
